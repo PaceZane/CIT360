55b549c71e3e2f2e5187ecb03e7f881bc62de47d
PaceZane <43303087+PaceZane@users.noreply.github.com>
2019-10-14 22:15:28 -0500

    Update JUnitDemo.java

--- JUNITDEMO/src/junitdemo/JUnitDemo.java
+++ JUNITDEMO/src/junitdemo/JUnitDemo.java
@@ -1,16 +1,6 @@
 package junitdemo;
 
-/**
- *
- * * @author Wayne Thomas sandbox code provided by
- * https://www.youtube.com/watch?v=KBx1pWhNUMc&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=FZVjICj_1vE&index=2&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=6RcjjpgOj5c&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=3
- * https://www.youtube.com/watch?v=skF3VZbfVqc&index=4&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=1H73r2RGlNQ&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=5
- * https://www.youtube.com/watch?v=iiroi_RWIpo&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=6
- * 
- */
+
 public class JUnitDemo {
 
     public boolean compareString(String first, String second) {
