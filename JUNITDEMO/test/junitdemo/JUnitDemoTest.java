20ebaa6737cb447a6f85ef7d960e47c7cd48135e
PaceZane <43303087+PaceZane@users.noreply.github.com>
2019-10-14 22:16:17 -0500

    Update JUnitDemoTest.java

--- JUNITDEMO/test/junitdemo/JUnitDemoTest.java
+++ JUNITDEMO/test/junitdemo/JUnitDemoTest.java
@@ -8,16 +8,7 @@ package junitdemo;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Wayne Thomas sandbox code provided by
- * https://www.youtube.com/watch?v=KBx1pWhNUMc&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=FZVjICj_1vE&index=2&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=6RcjjpgOj5c&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=3
- * https://www.youtube.com/watch?v=skF3VZbfVqc&index=4&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA
- * https://www.youtube.com/watch?v=1H73r2RGlNQ&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=5
- * https://www.youtube.com/watch?v=iiroi_RWIpo&list=PL_WCPOWW_gJEJkasFUTZEHSMFKi76_VXA&index=6
- */
+
 public class JUnitDemoTest {
 
     @Test
